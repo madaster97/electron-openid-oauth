f403fcce152cfdc80658c4bc6e9cef03fe1f5ec0
Adam Strickland <adamstrickland97@gmail.com>
2021-01-25 21:33:30 -0600

    refactor siop-client

--- frontend/lib/siop-client.js
+++ frontend/lib/siop-client.js
@@ -1,14 +1,26 @@
 const { Issuer } = require('openid-client');
+const fs = require('fs');
+const path = require('path');
+const sk = require('./pass-app/signing');
+const ek = require('./pass-app/encryption');
+const didPath = path.join(__dirname,'pass-app','did.txt');
+const didLong = fs.readFileSync(didPath,{
+    encoding: 'ascii'
+});
+
+sk.kid = `${didLong}#signing-key-1`;
+ek.kid = `${didLong}#encryption-key-1`;
+
+const jwks = {
+    key: [ek,sk]
+};
 
 const siop = new Issuer({
-    "authorization_endpoint": "openid:",
+    "authorization_endpoint": "http://localhost:8080",
     "issuer": "https://self-issued.me",
     "scopes_supported": [
         "openid",
-        "profile",
-        "email",
-        "address",
-        "phone"
+        "did_authn"
     ],
     "response_types_supported": [
         "id_token"
@@ -20,7 +32,6 @@ const siop = new Issuer({
         "RS256"
     ],
     "request_object_signing_alg_values_supported": [
-        "none",
         "RS256"
     ],
     "registration_endpoint": "https://self-issued.me/registration/1.0/"
@@ -39,27 +50,32 @@ const client = new siop.Client({
     request_object_signing_alg: "ECDH-ES",
     response_types: ["id_token"],
     ...siopRegistration
-});
+}, jwks);
 
-const verifierDid = "example:longform:did";
+const verifierDid = didLong;
 const scope = "openid did_authn";
 
-client.requestObject({
-    iss: verifierDid,
-    response_type: "id_token",
-    scope,
-    response_mode: "fragment",
-    registration: siopRegistration,
-    claims: {
-        id_token: {
-            "https://healthwallet.cards#covid19": { "essential": true },
+async function generateRequest() {
+    client.requestObject({
+        iss: verifierDid,
+        response_type: "id_token",
+        scope,
+        response_mode: "fragment",
+        registration: siopRegistration,
+        claims: {
+            id_token: {
+                "https://healthwallet.cards#covid19": { "essential": true },
+            }
         }
-    }
-})
-    .then(request => {
-        client.authorizationUrl({
+    }).then(request => {
+        return client.authorizationUrl({
             request,
             scope,
             client_id 
         })
     });
+}
+
+module.exports = {
+    generateRequest    
+};
